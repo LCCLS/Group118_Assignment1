744ac1f07f5aa8d25e6c7efd11737e341beb5c56
lclass <88320133+lclass@users.noreply.github.com>
2022-04-08 18:18:31 +0200

    preprocessing finished, plotting to do

--- Exploration.py
+++ Exploration.py
@@ -1,7 +1,7 @@
-import pprint
-import csv
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
+from dateutil import parser
 
 
 class Data_Exploration:
@@ -9,55 +9,139 @@ class Data_Exploration:
     def __init__(self, filename: str):
 
         # class vars
+
+        self.df = pd.DataFrame(pd.read_csv(filename, delimiter=";"))
         self.ODI = []
         self.ODI_frequencies = {}
         self.questions = []
         self.all_responses = []
 
-        # reading data
-        self.csv_reader(filename)
+        # clean data
 
-        #preprocessing of data --> needs to be done
-        self.preprocessing()
+        self.column_names()
+        self.preprocessing_birthdate()
+        self.preprocessing_competition_reward()
+        self.preprocessing_programme()
+        self.preprocessing_good_day_stress_ns()
 
-        #getting all answers and their frequencies
-        self.response_mapping()
-        #pprint.pprint(self.ODI_frequencies)
+    def column_names(self):
+        """
+        changes the lengthy column names to shorter versions.
 
-        #plotting the results
-        self.plot('Tijdstempel')
+        :return: a dataframe with updated column names
+        """
+        column_names = ['timestamp', 'program', 'course_on_ML', 'course_on_IR', 'course_on_stat', 'course_on_databases',
+                        'gender', 'chocolate_makes_you', 'birthday', 'nr_neighbor', 'stand_up', 'stress_lev',
+                        'competition reward', 'rand_num', 'bed_time_yesterday', 'good_day1', 'good_day2']
 
-    def csv_reader(self, input_file):
+        for name in enumerate(self.df.columns):
+            self.df.rename(columns={self.df.columns[name[0]]: column_names[name[0]]}, inplace=True)
 
-        with open(input_file, "r", encoding="utf-8-sig") as infile:
-            csv_reader = csv.reader(infile, delimiter=";")
-            for row in csv_reader:
-                self.ODI.append(row)
+    def preprocessing_birthdate(self):
+        """
+        preprocesses the birthdate column. All values are omitted that do not contain DD,MM,YY in some format
 
-    def preprocessing(self):
+        :return: a datetime format of all the birthdates in the column
+        """
 
-        # check what kind of preprocessing is needed and revise this method
-        # maybe use spacy to get tokens, handle missing values, etc.
+        for birthdate in self.df['birthday']:
+            if 8 <= len(birthdate) < 12 and birthdate != 'NaN':
+                try:
+                    if 1950 < parser.parse(birthdate).year < 2022:
+                        self.df['birthday'] = self.df['birthday'].replace(to_replace=birthdate,
+                                                                          value=parser.parse(birthdate))
+                    else:
+                        self.df['birthday'] = self.df['birthday'].replace(birthdate, 'NaN')
+                except ValueError:
+                    self.df['birthday'] = self.df['birthday'].replace(birthdate, 'NaN')
+            else:
+                self.df['birthday'] = self.df['birthday'].replace(birthdate, 'NaN')
 
-        pass
+    def preprocessing_competition_reward(self):
+        """
+        preprocesses competition reward. all values that are not numerical are omitted and only values between 0 and 101
+        are counted.
 
-    def response_mapping(self):
+        :return:a processes df column of values ranging from 0-100
+        """
 
-        self.questions = self.ODI[0]
-        self.all_responses = self.ODI[1:]
-        self.ODI_frequencies = {self.questions[i]: {} for i in range(len(self.questions))}
+        for response in self.df['competition reward']:
+            if type(response) != str or not response.isnumeric() or not 0 < int(response) < 101:
+                self.df['competition reward'] = self.df['competition reward'].replace(response, 'NaN')
 
-        for student in self.all_responses:
-            for response in enumerate(student):
+    def preprocessing_programme(self):
+        """
+        preprocesses the programme column and normalizes the names to standards. if the programme count is less than 3,
+        the programme is counted to the "others" categorisation
 
-                resp = response[1]
-                index = response[0]
+        :return: column of all the programmes of the students
+        """
 
-                if resp not in self.ODI_frequencies[self.questions[index]]:
-                    self.ODI_frequencies[self.questions[index]][resp] = 1
+        self.df["program"] = ["AI" if (x.lower() == "ai" or "artificial" in x.lower())
+                              else x for x in self.df["program"]]
+        self.df["program"] = ["CS" if ("computer" in x.lower() or "computational" in x.lower()) else x for x in
+                              self.df["program"]]
+        self.df["program"] = ["BA" if ("business" in x.lower()) else x for x in self.df["program"]]
+        top3 = ["AI", "BA", "CS"]
+        self.df["program"] = ["Other" if ((x not in top3) and (len(self.df[self.df.program == x]) < 3))
+                              else x for x in self.df["program"]]
 
-                elif resp in self.ODI_frequencies[self.questions[index]].keys():
-                    self.ODI_frequencies[self.questions[index]][resp] += 1
+    def preprocessing_yes_no(self):
+        """
+        preprocesses all the categorical answer options to standardised "0,1,unknown"
+
+        :return: the three columns with numerical values as indicators for responses
+        """
+
+        to_be_cleaned = ["course_on_ML", "course_on_stat", "course_on_databases"]
+        affirm = ["yes", "ja", "mu"]
+        deny = ["no", "sigma", "nee"]
+        for col in to_be_cleaned:
+            for x in self.df[col]:
+                if x in affirm:
+                    self.df[col] = self.df[col].replace(x, 1)
+                elif x in deny:
+                    self.df[col] = self.df[col].replace(x, 0)
+
+    def preprocessing_good_day_stress_ns(self):
+        """
+        processes the good days columns, the stress column, and the neighbours column.
+        Good days columns: all numerical answers are omitted
+        stress column: all values not within 0-100 are left out and non-numerical values omitted
+        neighbor columns: all non-numerical values are omitted, and values outside of 0-15 are left out
+
+        :return: cleaned df columns in self.df
+        """
+
+        clean_up = ["good_day1", "good_day2"]
+
+        self.df["stress_lev"] = [np.nan if (str(x).isnumeric() == False or int(x) > 100 or int(x) < 0) else x for x in
+                                 self.df["stress_lev"]]
+        self.df["nr_neighbor"] = [np.nan if (str(x).isnumeric() == False or int(x) > 15 or int(x) < 0) else x for x in
+                                  self.df["nr_neighbor"]]
+
+        for col in clean_up:
+            for x in self.df[col]:
+                if x.isnumeric():
+                    self.df[col] = self.df[col].replace(x, np.nan)
+
+    # def response_mapping(self):
+    #
+    #    self.questions = self.ODI[0]
+    #    self.all_responses = self.ODI[1:]
+    #    self.ODI_frequencies = {self.questions[i]: {} for i in range(len(self.questions))}
+    #
+    #    for student in self.all_responses:
+    #        for response in enumerate(student):
+    #
+    #            resp = response[1]
+    #            index = response[0]
+    #
+    #            if resp not in self.ODI_frequencies[self.questions[index]]:
+    #                self.ODI_frequencies[self.questions[index]][resp] = 1
+    #
+    #            elif resp in self.ODI_frequencies[self.questions[index]].keys():
+    #                self.ODI_frequencies[self.questions[index]][resp] += 1
 
     def plot(self, dict_key: str):
 
